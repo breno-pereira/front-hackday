68f2946b5f390b95258039790ef8c1605e65967e
marcio.silva <marcio.silva@redealumni.com>
2020-08-26 19:26:58 -0300

    add count signature

---
+++ functions/count-signature.js
@@ -0,0 +1,25 @@
+const faunadb = require('faunadb')
+
+const q = faunadb.query
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  /* construct the fauna query */
+  return client.query(q.Count(q.Match(q.Index("all_subscriptions"))))
+    .then((response) => {
+      /* Success! return the response with statusCode 200 */
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      })
+    }).catch((error) => {
+      /* Error! return the error with statusCode 400 */
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+}
