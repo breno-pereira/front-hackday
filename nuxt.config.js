b5534bb84dd4e8ba4039c77ced196c98871e0f0e
breno_pereira <brennohenrique_@live.com>
2020-08-26 11:44:14 -0300

    add configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,40 @@
+const modifyHtml = (html) => {
+  // Add amp-custom tag to added CSS and join all the CSS into one <style-tag>
+  let styleConcat = ''
+  html = html.replace(/(<style\b[^<>]*>)([^<]*)(<\/style>)/gi, (_match, p1, p2) => {
+    styleConcat += p2
+    return ''
+  })
+
+  html = html.replace('</head>', `<style amp-custom data-vue-ssr>${styleConcat}</style></head>`)
+  return html
+}
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
-    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'viewport', content: 'width=device-width,minimum-scale=1' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'canonical', href: '/' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
     ]
   },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
-  components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {},
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
+  css: ['~/assets/main.css'],
+  loading: false, // Disable loading bar since AMP will not generate a dynamic page
+  render: {
+    // Disable resourceHints since we don't load any scripts for AMP
+    resourceHints: false
+  },
+  hooks: {
+    // This hook is called before generatic static html files for SPA mode
+    'generate:page': (page) => {
+      page.html = modifyHtml(page.html)
+    },
+    // This hook is called before rendering the html to the browser
+    'render:route': (url, page, { req, res }) => {
+      page.html = modifyHtml(page.html)
+    }
   }
 }
